781364b96bf4dc77e0e805030c784285c128471d
pasindu681 <pasinduwitharana6@gmail.com>
2025-10-09 10:55:49 +0530

    About section add

---
+++ src/components/About.js
@@ -0,0 +1,38 @@
+import React from "react";
+import AboutBackground from "../Assets/about-background.png";
+import AboutBackgroundImage from "../Assets/about-background-image.png";
+import { BsFillPlayCircleFill } from "react-icons/bs";
+
+const About = () => {
+  return (
+    <div className="about-section-container">
+      <div className="about-background-image-container">
+        <img src={AboutBackground} alt="" />
+      </div>
+      <div className="about-section-image-container">
+        <img src={AboutBackgroundImage} alt="" />
+      </div>
+      <div className="about-section-text-container">
+        <p className="primary-subheading">About</p>
+        <h1 className="primary-heading">Good Food Supports a Balanced Life</h1>
+        <p className="primary-text">
+          A modern restaurant with a menu that will make your mouth water.
+          Servicing delicious food since nearly 10 years. Enjoy our seasonal
+          menu and experience the beauty of naturalness
+        </p>
+        <p className="primary-text">
+          Non tincidunt magna non et elit. Dolor turpis molestie dui magnis
+          facilisis at fringilla quam.
+        </p>
+        <div className="about-buttons-container">
+          <button className="secondary-button">Learn More</button>
+          <button className="watch-video-button">
+            <BsFillPlayCircleFill /> Watch Video
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
