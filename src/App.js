704f06dcd70e88e8a92bdadb9ba4967a43717a02
Pasidu Sumal <pasidugunarathna999@gmail.com>
2025-10-15 14:02:20 +0530

    update js error fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Home from "./components/Home";
-import About from "./components/About";
-import Work from "./components/Work";
-import Testimonial from "./components/Testimonial";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Work from "./Components/Work";
+import Testimonial from "./Components/Testimonial";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
