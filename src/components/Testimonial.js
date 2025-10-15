84b98114f7baef258cab6da8ec44ef9cc8064172
Pasidu Sumal <pasidugunarathna999@gmail.com>
2025-10-15 13:54:02 +0530

    Update Testimonial.js

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ProfilePic from "../Assets/john-doe-image.png";
-import { AiFillStar } from "react-icons/ai";
+import { AiFillStar } from "react-icons";
 
 const Testimonial = () => {
   return (
